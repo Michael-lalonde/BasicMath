ac5ef65f40758f81e334f49e58f981516553b48e
Michael Lalonde <mlalonde2310@gmail.com>
2025-03-21 22:21:32 -0600

    Add files via upload

---
+++ Test1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicMath;
+
+namespace BasicMathTests
+{
+    [TestClass]
+    public class BasicMathTests
+    {
+        private readonly BasicMath.BasicMath _basicMath = new BasicMath.BasicMath();
+
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            double result = _basicMath.Add(2, 3);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Test_SubtractMethod()
+        {
+            double result = _basicMath.Subtract(5, 2);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            double result = _basicMath.Multiply(3, 4);
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            double result = _basicMath.Divide(10, 2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            Assert.IsTrue(true); 
+        }
+    }
+}
